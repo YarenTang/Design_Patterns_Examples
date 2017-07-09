cb8a11e89f19c127377516ea452130b86d88c3e5
汤俞龙 <541976141@qq.com>
2017-07-09 13:29:15 +0800

    Java 懒汉单线程版

---
+++ src/Singleton/Java/LazySingletonOne.java
@@ -0,0 +1,20 @@
+package Singleton.Java;
+
+/**
+ * Created by prefert on 2017/7/3.
+ * <p>
+ * 懒汉模式 基础版（线程不安全
+ */
+public class LazySingletonOne {
+    private static LazySingletonOne instance;
+
+    private LazySingletonOne() {
+    }
+
+    public static LazySingletonOne getInstance() {
+        if (instance == null) {
+            instance = new LazySingletonOne();
+        }
+        return instance;
+    }
+}

34c3332411d79a0f3daefeb5a9c709f871279f00
汤俞龙 <541976141@qq.com>
2017-07-17 00:02:25 +0800

    update singleton test

--- src/Singleton/Scala/AppRegistry.scala
+++ src/Singleton/Scala/AppRegistry.scala
@@ -8,6 +8,7 @@ import scala.collection.mutable
   * Created by prefert on 2017/7/9.
   */
 object AppRegistry{
+
   println("Registry initialization block called.")
   private lazy val lazyXX  = ???
   private val users: scala.collection.mutable.HashMap[String, String] =  scala.collection.mutable.HashMap.empty

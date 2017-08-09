a04b0e666618a503308df4e771f54ed42a3f7805
汤俞龙 <541976141@qq.com>
2017-08-09 23:58:43 +0800

    Add Factory Method Pattern

---
+++ src/Factory/Method/Test.java
@@ -0,0 +1,61 @@
+package Factory.Method;
+
+import Factory.Traditional.*;
+
+
+/**
+ * Created by prefert on 2017/8/9.
+ */
+class Door {
+    public void getInfo() {
+        System.out.println("门");
+    }
+}
+
+class Wall {
+    public void getInfo() {
+        System.out.println("墙");
+    }
+}
+
+class Pillar {
+    public void getInfo() {
+        System.out.println("支柱");
+    }
+}
+
+interface IBuilding {
+    public void show();
+}
+class Building implements IBuilding {
+    private  Door door;
+    private  Wall wall;
+    private  Pillar pillar;
+    public Building( Door door, Wall wall, Pillar pillar) {
+        this.door = door;
+        this.wall = wall;
+        this.pillar = pillar;
+    }
+    public void show() {
+        System.out.println("房屋建好了");
+    }
+}
+
+interface IFactory {
+    public IBuilding createBuilding();
+}
+class Factory implements IFactory {
+    public IBuilding createBuilding() {
+        Door door = new Door();
+        Wall wall = new Wall();
+        Pillar pillar = new Pillar();
+        return new Building(door, wall, pillar);
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        IFactory factory = new Factory();
+        IBuilding building = factory.createBuilding();
+        building.show();
+    }
+}
---
+++ src/Factory/Traditional/Test.java
@@ -0,0 +1,53 @@
+package Factory.Traditional;
+
+/**
+ * Created by prefert on 2017/8/9.
+ */
+class Door {
+    public void getInfo() {
+        System.out.println("门");
+    }
+}
+
+class Wall {
+    public void getInfo() {
+        System.out.println("墙");
+    }
+}
+
+class Pillar {
+    public void getInfo() {
+        System.out.println("支柱");
+    }
+}
+
+interface IBuilding {
+    public void show();
+}
+
+class Building implements IBuilding {
+    private Door door;
+    private Wall wall;
+    private Pillar pillar;
+
+    public Building(Door door, Wall wall, Pillar pillar) {
+        this.door = door;
+        this.wall = wall;
+        this.pillar = pillar;
+    }
+
+    public void show() {
+        System.out.println("房屋建好了");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Door door = new Door();
+        Wall wall = new Wall();
+        Pillar pillar = new Pillar();
+
+        IBuilding building = new Building(door, wall, pillar);
+        building.show();
+    }
+}
